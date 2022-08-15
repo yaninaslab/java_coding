bbee7ce2540d102ce15b840fe312065d319aed50
yaninaslab <yana.belash@gmail.com>
2022-08-15 19:38:33 -0400

    My first steps in Java

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+      // Introducing ourselves and printing a tree
+      System.out.println("Hey there, I’m Yana and I’m learning to code in Java!");
+      System.out.println("I’m going to plant a tree today!");
+      System.out.println("    *  ");
+      System.out.println("   *** ");
+      System.out.println("  ***** ");
+      System.out.println(" ******** ");
+      System.out.println("**********");
+    }
+  }
