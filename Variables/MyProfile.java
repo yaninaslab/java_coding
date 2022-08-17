a4555084a715746b3de9c8ca6007605d2ba29fae
yaninaslab <yana.belash@gmail.com>
2022-08-17 13:02:03 -0400

    Variables practice

---
+++ Variables/MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Yanina Slabetska";
+        int age = 39;
+        double desiredSalary = 60000.75;
+        char gender = 'f';
+        boolean lookingForJob = true;
+
+    }
+}
