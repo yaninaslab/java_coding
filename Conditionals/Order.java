b6995241f78c459ae490ffad91a720529c1f9782
yaninaslab <yana.belash@gmail.com>
2022-09-08 14:06:32 -0400

    Conditionals and switch operator

--- Conditionals/Order.java
+++ Conditionals/Order.java
@@ -2,14 +2,16 @@ public class Order {
     boolean isFilled;
     double billAmount;
     String shipping;
+    String couponCode;
 
-    public Order(boolean filled, double cost, String shippingMethod) {
+    public Order(boolean filled, double cost, String shippingMethod, String coupon) {
         if (cost > 24.00) {
             System.out.println("High value item!");
         }
         isFilled = filled;
         billAmount = cost;
         shipping = shippingMethod;
+        couponCode = coupon;
     }
 
     public void ship() {
@@ -22,20 +24,39 @@ public class Order {
     }
 
     public double calculateShipping() {
-        // declare conditional statement here
         if (shipping.equals("Regular")) {
             return 0;
         } else if (shipping.equals("Express")) {
-            return 1.75;
+            if (couponCode.equals("ship50")) {
+                return 0.85;
+            } else {
+                return 1.75;
+            }
         } else {
-            return 0.50;
+            return .50;
         }
     }
 
+    // public double calculateShipping() {
+    // double shippingCost;
+    // switch (shipping) {
+    // case "Regular":
+    // shippingCost = 0;
+    // break;
+    // case "Express":
+    // shippingCost = 1.75;
+    // break;
+    // default:
+    // shippingCost = 0.50;
+    // }
+
+    // return shippingCost;
+    // }
+
     public static void main(String[] args) {
         // do not alter the main method!
-        Order book = new Order(true, 9.99, "Express");
-        Order chemistrySet = new Order(false, 72.50, "Regular");
+        Order book = new Order(true, 9.99, "Express", "ship50");
+        Order chemistrySet = new Order(false, 72.50, "Regular", "freeShipping");
 
         book.ship();
         chemistrySet.ship();
