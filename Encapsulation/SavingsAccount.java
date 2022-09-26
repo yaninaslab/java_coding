b912d977f3bc1f38a58cc6de3149add5af5bab6f
yaninaslab <yana.belash@gmail.com>
2022-09-25 23:33:30 -0400

    Static methods and variables

---
+++ Encapsulation/SavingsAccount.java
@@ -0,0 +1,39 @@
+public class SavingsAccount {
+
+    public String owner;
+    public int balanceDollar;
+    public double balanceEuro;
+
+    public SavingsAccount(String owner, int balanceDollar) {
+        // Complete the constructor
+        this.owner = owner;
+        this.balanceDollar = balanceDollar;
+        this.balanceEuro = balanceDollar * 0.85;
+
+    }
+
+    // public void addMoney(int balanceDollar){
+    // // Complete this method
+    // this.balanceDollar += balanceDollar;
+    // System.out.println("Adding " + balanceDollar + " dollars to the account.");
+    // System.out.println("The new balance is " + this.balanceDollar + " dollars");
+    // }
+
+    public void addMoney(int balanceEuro) {
+        // Complete this method
+        this.balanceEuro += balanceEuro;
+        System.out.println("Adding " + balanceEuro + " dollars to the account.");
+        System.out.println("The new balance is " + this.balanceEuro + " dollars");
+    }
+
+    public static void main(String[] args) {
+        SavingsAccount zeusSavingsAccount = new SavingsAccount("Zeus", 1000);
+
+        // Make a call to addMoney() to test your method
+
+        // zeusSavingsAccount.addMoney(2000);
+        zeusSavingsAccount.addMoney(1000);
+
+    }
+
+}
